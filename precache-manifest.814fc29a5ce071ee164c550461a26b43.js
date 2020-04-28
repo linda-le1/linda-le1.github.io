b953b150b9db1cd2d74c21fb3c7c8e975732a0a5
Linda Le <yueling1222@gmail.com>
2020-04-28 01:36:55 -0600

    Updates

---
+++ precache-manifest.814fc29a5ce071ee164c550461a26b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "285c8a888c564922c3c6",
+    "url": "/portfolio/static/css/main.0b18d711.chunk.css"
+  },
+  {
+    "revision": "285c8a888c564922c3c6",
+    "url": "/portfolio/static/js/main.285c8a88.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/portfolio/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "7049b191670b08156d7418e88bd4cb66",
+    "url": "/portfolio/static/media/Linda_Le_Resume.7049b191.pdf"
+  },
+  {
+    "revision": "259505625552e836898238414e7cb27d",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.687e98206f898f4ea8c7c9e41378aa48.js"
+  "/portfolio/precache-manifest.814fc29a5ce071ee164c550461a26b43.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
