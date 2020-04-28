1e486dd0c88cc416816c305eecb3fb644a9f530f
Linda Le <yueling1222@gmail.com>
2020-04-27 18:23:57 -0600

    Updates

---
+++ precache-manifest.dae3a85aa714497be9ce117941fe144b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c1f88055c028332f9ebc6e7e6a4445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a755eba1eecffae0021d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afda21390b6b4cb282a8",
+    "url": "/static/js/2.262eeec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.262eeec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a755eba1eecffae0021d",
+    "url": "/static/js/main.61c944c3.chunk.js"
+  },
+  {
+    "revision": "b775e3132e0930477ff9",
+    "url": "/static/js/runtime-main.17a0efde.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
