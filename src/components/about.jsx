c65407a46fad2363876f66b83530722db1976f9d
Linda Le <yueling1222@gmail.com>
2020-04-28 02:06:20 -0600

    Update about me section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,36 +17,39 @@ export default class About extends Component {
                       <span className="heading-meta">About</span>
                       <h2 className="colorlib-heading">Who Am I?</h2>
                       <p>
-                        I'm a back-end developer based in Denver, Colorado. I
-                        have a passion for data, testing, and functionality.
+                        I'm a highly motivated, empathetic, and conscientious
+                        back-end developer based in Denver, Colorado. I have a
+                        passion for data, testing, and functionality.
                       </p>
                       <p>
                         Before I became a programmer, I worked in Higher
                         Education. I spent the majority of my time working with
                         international students and later transitioned into
-                        managing communications for student housing. Collaborating
-                        with a wide variety of stakeholders made me learn how to
-                        communicate complex topics simply and solve problems creatively.
+                        managing communications for student housing.
+                        Collaborating with a wide variety of stakeholders made
+                        me learn how to communicate complex topics simply.
                       </p>
                       <p>
                         After I earned my Master's in Information Communications
-                        Technology, I sought out a more technical role.
-                        This led me to becoming an SEO Strategist at a HubSpot
-                        Diamond Partner agency. While I really enjoyed researching and
-                        implementing ethical and sound SEO strategies for a diverse
-                        set of clients, I decided to pivot again.
+                        Technology, I sought out a more technical role. This led
+                        me to becoming an SEO Strategist at a HubSpot Diamond
+                        Partner agency. I enjoyed being able to build sound,
+                        ethical SEO strategies and see the impact of my work.
+                        However, I still felt like something was missing.
                       </p>
                       <p>
                         My lifelong interest in technology, love of languages,
-                        and desire to creatively problem solve led me to pursue
+                        and desire to creatively solve problems led me to pursue
                         programming and enroll at Turing School of Software and
                         Design. Turing is a 7-month, in-person, intensive
-                        programming school. I'm a highly motivated, empathetic,
-                        and conscientious back-end developer with experience in
-                        Ruby on Rails.
+                        programming school. Throughout the program, I learned
+                        new programming languages, tools, and technologies
+                        quickly. Most of my programming experience is in Ruby on
+                        Rails. I'm currently learning Python and Javascript.
                       </p>
-                      <p>Outside of coding, I'm an avid traveler, huge foodie, and
-                          caffeine addict.
+                      <p>
+                        Outside of coding, I'm an avid traveler, huge foodie,
+                        and caffeine addict.
                       </p>
                     </div>
                   </div>
