897bffd1cb4f190f8a7e01d634c98eeb21c2018d
Linda Le <yueling1222@gmail.com>
2020-04-27 22:58:22 -0600

    Updates

---
+++ precache-manifest.103ff7e23c3949bfac1062846bab9c36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c382c8b0325ac5cd82a6",
+    "url": "/static/css/main.d2bc8ba2.chunk.css"
+  },
+  {
+    "revision": "c382c8b0325ac5cd82a6",
+    "url": "/static/js/main.c382c8b0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "33cf45e9a3cd81ea103565adee759b7f",
+    "url": "/index.html"
+  }
+];
