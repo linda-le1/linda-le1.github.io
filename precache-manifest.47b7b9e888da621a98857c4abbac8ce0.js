a869a43bae12f4d0b73c9dc6acf90ec523b5c204
Linda Le <yueling1222@gmail.com>
2020-04-29 16:34:08 -0600

    Updates

--- precache-manifest.47b7b9e888da621a98857c4abbac8ce0.js
+++ precache-manifest.47b7b9e888da621a98857c4abbac8ce0.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "87e991a9454d8211e623",
+    "revision": "1153962a3bfaba89f683",
     "url": "/portfolio/static/css/main.0b18d711.chunk.css"
   },
   {
-    "revision": "87e991a9454d8211e623",
-    "url": "/portfolio/static/js/main.87e991a9.chunk.js"
+    "revision": "1153962a3bfaba89f683",
+    "url": "/portfolio/static/js/main.1153962a.chunk.js"
   },
   {
     "revision": "0fad4c0f10d57b530fe4",
     "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
   },
   {
-    "revision": "98da1cabb003c3e4b41b",
-    "url": "/portfolio/static/js/2.98da1cab.chunk.js"
+    "revision": "5f7464ecbdac078505f3",
+    "url": "/portfolio/static/js/2.5f7464ec.chunk.js"
   },
   {
     "revision": "7049b191670b08156d7418e88bd4cb66",
     "url": "/portfolio/static/media/Linda_Le_Resume.7049b191.pdf"
   },
   {
-    "revision": "8a2df0340bcc59621afb3c44b31ef057",
+    "revision": "5cd6c81cdf9ed09eae0d47f54d0606f8",
     "url": "/portfolio/index.html"
   }
 ];
